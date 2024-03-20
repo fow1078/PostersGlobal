66569579f48eaa06a857ab2f5eed7b4f9058cb50
webdevswapter <152205332+webdevswapter@users.noreply.github.com>
2024-03-20 23:32:34 +0100

    Fix

--- netlify/functions/sendMail/sendMail.js
+++ netlify/functions/sendMail/sendMail.js
@@ -1,17 +1,46 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+// // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// const handler = async (event) => {
+//   try {
+//     const subject = event.queryStringParameters.name || 'World'
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: `Hello ${subject}` }),
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     }
+//   } catch (error) {
+//     return { statusCode: 500, body: error.toString() }
+//   }
+// }
 
-module.exports = { handler }
+// module.exports = { handler }
+
+const mailgun = require("mailgun-js");
+
+exports.handler = async (event, context) => {
+    const DOMAIN = "sandboxa20a34cc13ba4c6babb98c99e1b7c3fc.mailgun.org";
+    const mg = mailgun({apiKey: "44f43e9157921d644260c3639482ae7c-309b0ef4-54de112b", domain: DOMAIN});
+
+    const emailData = {
+        from: "Helper <posters@sandboxa20a34cc13ba4c6babb98c99e1b7c3fc.mailgun.org>",
+        to: 'fow1078@gmail.com',
+        subject: 'Contact Us Request',
+        text: "Name: fkfkrfmfrr"
+    };
+
+    try {
+        const body = await mg.messages().send(emailData);
+        console.log(body)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        };
+    } catch (error) {
+        console.log(error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        };
+    }
+};
