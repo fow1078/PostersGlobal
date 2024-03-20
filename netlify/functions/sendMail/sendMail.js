a76e9f77bbd3dfdaee553c06e5078511f61fc2a1
webdevswapter <152205332+webdevswapter@users.noreply.github.com>
2024-03-21 00:09:24 +0100

    Fix

--- netlify/functions/sendMail/sendMail.js
+++ netlify/functions/sendMail/sendMail.js
@@ -16,31 +16,25 @@
 
 // module.exports = { handler }
 
-const mailgun = require("mailgun-js");
-
 exports.handler = async (event, context) => {
-    const DOMAIN = "sandboxa20a34cc13ba4c6babb98c99e1b7c3fc.mailgun.org";
-    const mg = mailgun({apiKey: "44f43e9157921d644260c3639482ae7c-309b0ef4-54de112b", domain: DOMAIN});
+  const API_KEY = '787934F3EDA1952BCC3D49463EF7287CD75E6EC60CC65446E199D36826028F6A6E7FE2816431DC4452BCEF990C6B084F'; // Replace with your API key
+  const SENDER_EMAIL = 'fow1078@gmail.com'; // This email must be verified with Elastic Email
 
-    const emailData = {
-        from: "Helper <posters@sandboxa20a34cc13ba4c6babb98c99e1b7c3fc.mailgun.org>",
-        to: 'fow1078@gmail.com',
-        subject: 'Contact Us Request',
-        text: "Name: fkfkrfmfrr"
-    };
+  const data = new URLSearchParams({
+    'apikey': API_KEY,
+    'subject': subject,
+    'from': SENDER_EMAIL,
+    'to': 'cristarium11@gmail.com',
+    'bodyHtml': 'test',
+    'isTransactional': true
+  });
 
-    try {
-        const body = await mg.messages().send(emailData);
-        console.log(body)
-        return {
-            statusCode: 200,
-            body: JSON.stringify(body)
-        };
-    } catch (error) {
-        console.log(error)
-        return {
-            statusCode: 500,
-            body: JSON.stringify(error)
-        };
-    }
+  try {
+    const response = await axios.post('https://api.elasticemail.com/v2/email/send', data);
+    console.log(response.data);
+    // Handle response here
+  } catch (error) {
+    console.error(error);
+    // Handle error here
+  }
 };
